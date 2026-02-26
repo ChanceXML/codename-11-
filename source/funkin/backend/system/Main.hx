e5584651b423b97650e27a3a66361780066fa376
ChanceXML <joseguest1337@gmail.com>
2026-02-26 14:08:08 -0700

    Update Main.hx

--- source/funkin/backend/system/Main.hx
+++ source/funkin/backend/system/Main.hx
@@ -1,5 +1,6 @@
 package funkin.backend.system;
 
+import flixel.FlxG;
 import flixel.addons.transition.FlxTransitionSprite.GraphicTransTileDiamond;
 import flixel.addons.transition.FlxTransitionableState;
 import flixel.addons.transition.TransitionData;
@@ -19,19 +20,21 @@ import funkin.options.PlayerSettings;
 import openfl.Assets;
 import openfl.Lib;
 import openfl.display.Sprite;
-import openfl.text.TextFormat;
 import openfl.utils.AssetLibrary;
 import sys.FileSystem;
 import sys.io.File;
+
 #if android
-import android.content.Context;
+import android.Manifest
 import android.os.Build;
+import lime.system.System;
 import funkin.mobile.utils.MobileUtil;
 #end
 
 class Main extends Sprite
 {
 	public static var instance:Main;
+	public static var externalRoot:String;
 
 	public static var modToLoad:String = null;
 	public static var forceGPUOnlyBitmapsOff:Bool = #if desktop false #else true #end;
@@ -39,24 +42,20 @@ class Main extends Sprite
 	public static var verbose:Bool = false;
 
 	public static var scaleMode:FunkinRatioScaleMode;
-	#if !mobile
+
+	#if desktop
 	public static var framerateSprite:Framerate;
 	#end
 
-	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels).
-	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels).
-	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
-	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
+	var gameWidth:Int = 1280;
+	var gameHeight:Int = 720;
+	var skipSplash:Bool = true;
+	var startFullscreen:Bool = false;
 
 	public static var game:FunkinGame;
 
-	/**
-	 * The time since the game was focused last time in seconds.
-	 */
 	public static var timeSinceFocus(get, never):Float;
 	public static var time:Int = 0;
-
-	// You can pretty much ignore everything from here on - your code should go in your states.
 
 	public static function preInit() {
 		funkin.backend.utils.NativeAPI.registerAsDPICompatible();
@@ -67,22 +66,41 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-
 		instance = this;
-
 		CrashHandler.init();
-
+		initStorage();
 		addChild(game = new FunkinGame(gameWidth, gameHeight, MainState, Options.framerate, Options.framerate, skipSplash, startFullscreen));
-
-		// fps. which were disabled for android for some reason
+		#if desktop
 		addChild(framerateSprite = new Framerate());
 		SystemInfo.init();
+		#end
 	}
 
-	@:dox(hide)
-	public static var audioDisconnected:Bool = false;
+	function initStorage()
+	{
+		#if android
+		if (Build.VERSION.SDK_INT < 33)
+		{
+			System.requestPermissions([
+				Manifest.permission.READ_EXTERNAL_STORAGE,
+				Manifest.permission.WRITE_EXTERNAL_STORAGE
+			]);
+		}
 
+		externalRoot = "/storage/emulated/0/.CodenameEngine-v1.0.1/";
+
+		if (!FileSystem.exists(externalRoot))
+			FileSystem.createDirectory(externalRoot);
+		#else
+		externalRoot = ".CodenameEngine-v1.0.1/";
+		if (!FileSystem.exists(externalRoot))
+			FileSystem.createDirectory(externalRoot);
+		#end
+	}
+
+	public static var audioDisconnected:Bool = false;
 	public static var changeID:Int = 0;
+
 	public static var pathBack = #if (windows || linux)
 			"../../../../"
 		#elseif mac
@@ -90,9 +108,9 @@ class Main extends Sprite
 		#else
 			"../../../../"
 		#end;
+
 	public static var startedFromSource:Bool = #if TEST_BUILD true #else false #end;
 
-	// DEPRECATED
 	@:dox(hide) public static function execAsync(func:Void->Void) ThreadUtil.execAsync(func);
 
 	private static function getTimer():Int {
@@ -100,24 +118,27 @@ class Main extends Sprite
 	}
 
 	public static function loadGameSettings() {
-        // Android Permissions
+
 		#if android
-        MobileUtil.getPermissions();
-        MobileUtil.copyAssetsFromAPK();
-        MobileUtil.copyModsFromAPK();
-        #end
-			
+		MobileUtil.getPermissions();
+		MobileUtil.copyAssetsFromAPK();
+		MobileUtil.copyModsFromAPK();
+		#end
+
 		WindowUtils.init();
 		SaveWarning.init();
 		MemoryUtil.init();
+
 		@:privateAccess
 		FlxG.game.getTimer = getTimer;
+
 		FunkinCache.init();
 		Paths.assetsTree = new AssetsLibraryList();
 
 		#if UPDATE_CHECKING
 		funkin.backend.system.updating.UpdateUtil.init();
 		#end
+
 		ShaderResizeFix.init();
 		Logs.init();
 		Paths.init();
@@ -133,27 +154,25 @@ class Main extends Sprite
 		lib.__proxy = Paths.assetsTree;
 		Assets.registerLibrary('default', lib);
 
-		funkin.options.PlayerSettings.init();
+		PlayerSettings.init();
 		Options.load();
 
 		FlxG.fixedTimestep = false;
-
 		FlxG.scaleMode = scaleMode = new FunkinRatioScaleMode();
 
 		Conductor.init();
 		AudioSwitchFix.init();
 		EventManager.init();
+
 		FlxG.signals.focusGained.add(onFocus);
 		FlxG.signals.preStateSwitch.add(onStateSwitch);
 		FlxG.signals.postStateSwitch.add(onStateSwitchPost);
 		FlxG.signals.postUpdate.add(onUpdate);
 
 		FlxG.mouse.useSystemCursor = true;
-		#if DARK_MODE_WINDOW
-		if(funkin.backend.utils.NativeAPI.hasVersion("Windows 10")) funkin.backend.utils.NativeAPI.redrawWindowHeader();
-		#end
 
 		ModsFolder.init();
+
 		#if MOD_SUPPORT
 		if (FileSystem.exists("mods/autoload.txt"))
 			modToLoad = File.getContent("mods/autoload.txt").trim();
@@ -164,24 +183,6 @@ class Main extends Sprite
 		initTransition();
 	}
 
-	public static function refreshAssets() @:privateAccess {
-		FunkinCache.instance.clearSecondLayer();
-
-		var game = FlxG.game;
-		var daSndTray = Type.createInstance(game._customSoundTray = funkin.menus.ui.FunkinSoundTray, []);
-		var index:Int = game.numChildren - 1;
-
-		if(game.soundTray != null)
-		{
-			var newIndex:Int = game.getChildIndex(game.soundTray);
-			if(newIndex != -1) index = newIndex;
-			game.removeChild(game.soundTray);
-			game.soundTray.__cleanup();
-		}
-
-		game.addChildAt(game.soundTray = daSndTray, index);
-	}
-
 	public static function initTransition() {
 		var diamond:FlxGraphic = FlxGraphic.fromClass(GraphicTransTileDiamond);
 		diamond.persist = true;
@@ -189,6 +190,7 @@ class Main extends Sprite
 
 		FlxTransitionableState.defaultTransIn = new TransitionData(FADE, 0xFF000000, 1, new FlxPoint(0, -1), {asset: diamond, width: 32, height: 32},
 			new FlxRect(-200, -200, FlxG.width * 1.4, FlxG.height * 1.4));
+
 		FlxTransitionableState.defaultTransOut = new TransitionData(FADE, 0xFF000000, 0.7, new FlxPoint(0, 1),
 			{asset: diamond, width: 32, height: 32}, new FlxRect(-200, -200, FlxG.width * 1.4, FlxG.height * 1.4));
 	}
@@ -210,36 +212,28 @@ class Main extends Sprite
 	}
 
 	private static function onStateSwitchPost() {
-		// manual asset clearing since base openfl one does'nt clear lime one
-		// does'nt clear bitmaps since flixel fork does it auto
-
 		@:privateAccess {
-			// clear uint8 pools
 			for(length=>pool in openfl.display3D.utils.UInt8Buff._pools) {
 				for(b in pool.clear())
 					b.destroy();
 			}
-
 			openfl.display3D.utils.UInt8Buff._pools.clear();
 		}
-
 		MemoryUtil.clearMajor();
 	}
 
 	public static var noCwdFix:Bool = false;
+
 	public static function fixWorkingDirectory() {
 		#if windows
 		if (!noCwdFix && !sys.FileSystem.exists('manifest/default.json')) {
 			Sys.setCwd(haxe.io.Path.directory(Sys.programPath()));
 		}
-		#elseif android
-		Sys.setCwd(haxe.io.Path.addTrailingSlash(VERSION.SDK_INT > 30 ? Context.getObbDir() : Context.getExternalFilesDir()));
-		#elseif (ios || switch)
-		Sys.setCwd(haxe.io.Path.addTrailingSlash(openfl.filesystem.File.applicationStorageDirectory.nativePath));
 		#end
 	}
 
 	private static var _tickFocused:Float = 0;
+
 	public static function get_timeSinceFocus():Float {
 		return (FlxG.game.ticks - _tickFocused) / 1000;
 	}
